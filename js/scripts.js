8f129b6e93f52fc74457f8f6887f865abc36ed77
Maggie Summers <summers.maggie@gmail.com>
2018-01-23 19:24:29 -0800

    refactor JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,57 @@
-//two submit functions for each form
-//initial deposit will be global in business
+//biz logic
+
+
+// //for if statement have amount === 0. otherwise it'll be undefined.
+// BankAccount.prototype.withdrawal = function() {
+//   this.balance -= withdrawal;
+// }
+// BankAccount.prototype.deposit = function() {
+//   this.balance += deposit;
+// }
+
 var newBankAccount;
+
 function BankAccount(name, balance) {
   this.name = name;
   this.balance = balance;
-
 }
-//for if statement have amount === 0. otherwise it'll be undefined.
-
-BankAccount.prototype.withdrawal = function() {
-  this.balance -= withdrawal;
-}
-
-BankAccount.prototype.deposit = function() {
-  this.balance += deposit;
-}
-
-// function resetFields() {
-//   $("input#deposit").val();
-//   $("input#name").val();
-//   $("input#initialDeposit").val();
-//   $("input#withdrawal").val();
-// }
 
 //ui logic
-var account = [];
-
 $(document).ready(function() {
-    var initialDeposit = [];
-
-  $("form.bank").submit(function(event) {
+  $("form#bank").submit(function(event) {
     event.preventDefault();
+
     var name = $("input#name").val();
     var initialDeposit = parseInt($("input#initialDeposit").val());
 
-    newBankAccount = BankAccount(name, initialDeposit);
+    newBankAccount = new BankAccount(name, initialDeposit);
+    console.log(newBankAccount)
 
-    $(".amount").text(newBankAccount.name + ", your bank balance is " + newBankAccount.balance);
+    $("form#depoWith").fadeIn();
+    $("#firstresult").text(name + ", your bank balance is $" + initialDeposit + ".");
   });
 
-  $("form.depoWith").submit(function(event) {
+  $("form#depoWith").submit(function(event) {
     event.preventDefault();
+
     var deposit = parseInt($("input#deposit").val());
+    console.log(deposit);
     var withdrawal = parseInt($("input#withdrawal").val());
+    console.log(withdrawal);
 
-    newBankAccount.balance = newBankAccount.balance - deposit;
-    newBankAccount.balance = newBankAccount.balance + deposit;
-
-    $(".amount").text(newBankAccount.amount);
+    newBankAccount.balance =  + deposit;
+    console.log(depositTotal);
   });
+});
+
+
+
+
+  //   newBankAccount.balance = newBankAccount.balance + deposit;
+  //   newBankAccount.balance = newBankAccount.balance - withdrawal;
+
+    //$(".amount").text(newBankAccount.amount);
 
   // $(".amount").text(finalAmount)
-
-  resetFields();
-});
+  //
+  // });
