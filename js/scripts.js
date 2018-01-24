151f75f0459eeeabb9b250f43294ea5d837079c9
Maggie Summers <summers.maggie@gmail.com>
2018-01-23 19:43:35 -0800

    finalize JS code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
 //biz logic
-
-
-// //for if statement have amount === 0. otherwise it'll be undefined.
-// BankAccount.prototype.withdrawal = function() {
-//   this.balance -= withdrawal;
-// }
-// BankAccount.prototype.deposit = function() {
-//   this.balance += deposit;
-// }
-
 var newBankAccount;
 
 function BankAccount(name, balance) {
@@ -25,8 +15,7 @@ $(document).ready(function() {
     var initialDeposit = parseInt($("input#initialDeposit").val());
 
     newBankAccount = new BankAccount(name, initialDeposit);
-    console.log(newBankAccount)
-
+    
     $("form#depoWith").fadeIn();
     $("#firstresult").text(name + ", your bank balance is $" + initialDeposit + ".");
   });
@@ -35,24 +24,13 @@ $(document).ready(function() {
     event.preventDefault();
 
     var deposit = parseInt($("input#deposit").val());
-    console.log(deposit);
     var withdrawal = parseInt($("input#withdrawal").val());
-    console.log(withdrawal);
 
     newBankAccount.balance = newBankAccount.balance + deposit;
     newBankAccount.balance = newBankAccount.balance - withdrawal;
-    console.log(newBankAccount.balance);
+
+    $("#secondresult").text("Your new current balance is " + "$" + newBankAccount.balance + ".")
+
+    $("#firstresult").hide();
   });
 });
-
-
-
-
-  //   newBankAccount.balance = newBankAccount.balance + deposit;
-  //   newBankAccount.balance = newBankAccount.balance - withdrawal;
-
-    //$(".amount").text(newBankAccount.amount);
-
-  // $(".amount").text(finalAmount)
-  //
-  // });
